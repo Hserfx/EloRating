d641ade29018a8003dbe754f82ff5d38307644ff
hserfx <mleszek.kontaktowy@gmail.com>
2024-06-03 20:34:43 +0200

    port 80

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import uvicorn
 from elo import update_scores
 
 class Player(BaseModel):
@@ -15,3 +16,6 @@ app = FastAPI()
 def update_elo(players: List[Player]):
     response = update_scores(players)
     return response
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0'. port=80)
