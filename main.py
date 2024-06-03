ae82a727b7d726908b6b52553f74e526ed82e13a
hserfx <mleszek.kontaktowy@gmail.com>
2024-06-03 21:11:07 +0200

    uvicorn

--- main.py
+++ main.py
@@ -18,5 +18,6 @@ def update_elo(players: List[Player]):
     response = update_scores(players)
     return response
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=os.getenv('PORT', default=8000))
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
